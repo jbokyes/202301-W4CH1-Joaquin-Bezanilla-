591695c594091a2a3d4bfc3ba93a23cb690c4479
Joaquin Bezanilla <joaquinbezanilla@gmail.com>
2023-02-06 20:53:50 +0100

    Added header

--- src/components/app/App.tsx
+++ src/components/app/App.tsx
@@ -2,23 +2,27 @@ import React from "react";
 import "./App.css";
 import { Gentleman } from "../gentleman/gentleman";
 import { gentlemenMock } from "../../mocks/gentleman";
+import { Info } from "../info/info";
 
 function App() {
   return (
-    <main className="main">
-      <ul className="gentlemen">
-        {gentlemenMock.map((elem) => (
-          <Gentleman
-            name={elem.name}
-            status={elem.status}
-            profession={elem.profession}
-            twitter={elem.twitter}
-            picture={elem.picture}
-            alternativeText={elem.alternativeText}
-          ></Gentleman>
-        ))}
-      </ul>
-    </main>
+    <>
+      <Info></Info>
+      <main className="main">
+        <ul className="gentlemen">
+          {gentlemenMock.map((elem) => (
+            <Gentleman
+              name={elem.name}
+              status={elem.status}
+              profession={elem.profession}
+              twitter={elem.twitter}
+              picture={elem.picture}
+              alternativeText={elem.alternativeText}
+            ></Gentleman>
+          ))}
+        </ul>
+      </main>
+    </>
   );
 }
 
