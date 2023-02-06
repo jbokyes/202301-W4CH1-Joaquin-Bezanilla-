40bc69b4a4b48d734bfd7aca4ee78c384445c278
Joaquin Bezanilla <joaquinbezanilla@gmail.com>
2023-02-06 20:17:55 +0100

    finished app function

--- src/components/app/App.tsx
+++ src/components/app/App.tsx
@@ -1,20 +1,25 @@
 import React from "react";
-import logo from "./logo.svg";
 import "./App.css";
 import { Gentleman } from "../gentleman/gentleman";
 import { gentlemenMock } from "../../mocks/gentleman";
 
 function App() {
-  return gentlemenMock.map((elem) => (
-    <Gentleman
-      name={elem.name}
-      status={elem.status}
-      profession={elem.profession}
-      twitter={elem.twitter}
-      picture={elem.picture}
-      alternativeText={elem.alternativeText}
-    ></Gentleman>
-  ));
+  return (
+    <main className="main">
+      <ul className="gentlemen">
+        {gentlemenMock.map((elem) => (
+          <Gentleman
+            name={elem.name}
+            status={elem.status}
+            profession={elem.profession}
+            twitter={elem.twitter}
+            picture={elem.picture}
+            alternativeText={elem.alternativeText}
+          ></Gentleman>
+        ))}
+      </ul>
+    </main>
+  );
 }
 
 export default App;
